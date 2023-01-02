3a9ccf4915bfe961b14fcc9ac3226c473be3589b
Ignacio Sanchez Gines <nacho@geardome.com>
2023-01-02 21:01:27 +0100

    Add more sensors

--- Dockerfile
+++ Dockerfile
@@ -2,9 +2,12 @@ FROM python:latest
 
 WORKDIR /app
 
+RUN adduser -u 5678 --disabled-password --gecos "" appuser && chown -R appuser /app
+USER appuser
+
 COPY requirements.txt requirements.txt
 RUN pip install -r requirements.txt
 
-COPY gather_data.py gather_data.p
+COPY gather_data.py gather_data.py
 
 CMD [ "python", "gather_data.py"]
--- gather_data.py
+++ gather_data.py
@@ -1,19 +1,27 @@
-from pymodbus.constants import Defaults
 from pymodbus.client import ModbusTcpClient
 from pymodbus.constants import Endian
 from pymodbus.payload import BinaryPayloadDecoder
-import subprocess
-import json
+from influxdb_client import InfluxDBClient, Point
+from influxdb_client.client.write_api import SYNCHRONOUS
 import time
-import pdb
-import os
 
-scheduling_seconds = 10.0
+scheduling_seconds = 6.0
 
 inverter_host = "192.168.200.1"
 inverter_port = 6607
 inverter_id = 0
 
+modbus_wait_for_connnection = 2.0
+
+influx_url = "http://192.168.1.95:8086"
+influx_token = "nHFF0UL3kgcjWEvUjvxeUrjS7DfJFzdohW22o0VaYGSs2SH6wvbQAMJXO1uf6TgH9BDS0N-gLJ45lvYbjbd2HA=="
+influx_org = "sun2000"
+measurement = "sun2000"
+measurement_version = "1.0"
+bucket = "sensors"
+
+inverter_starting_address = 32000
+inverter_register_count = 116
 inverter_sensors = [
   ["inverter_status_1", "uint16", 0, 1, 0, "Inverter Status 1", 0],
   ["inverter_status_2", "uint16", 2, 1, 0, "Inverter Status 2", 0],
@@ -26,15 +34,21 @@ inverter_sensors = [
   ["inverter_dc_p", "int32", 64, 2, 0.001, "Inverter DC Power (kW)", 0],
   ["inverter_ac_v", "uint16", 66, 1, 0.1, "Inverter AC Voltage (V)", 0],
   ["inverter_ac_a", "int32", 72, 2, 0.001, "Inverter AC Current (A)", 0],
+  ["inverter_ac_p_peak", "int32", 78, 2, 0.001, "Inverter AC Power Peak (kW)", 0],
   ["inverter_ac_p", "int32", 80, 2, 0.001, "Inverter AC Power (kW)", 0],
+  ["inverter_ac_reactive_p", "int32", 82, 2, 0.001, "Inverter AC Reactive Power (kVar)", 0],
   ["inverter_pf", "int16", 84, 1, 0.001, "Inverter Power Factor", 0],
   ["inverter_f", "uint16", 85, 1, 0.01, "Inverter Frequency (Hz)", 0],
   ["inverter_efficiency", "uint16", 86, 1, 0.01, "Inverter Efficiency (%)", 0],
   ["inverter_temp", "int16", 87, 1, 0.1, "Inverter Temperature (C)", 0],
   ["inverter_insulation_r", "uint16", 88, 1, 0.001, "Inverter Insulation Resistency (Mohms)", 0],
-  ["inverter_device_status", "uint16", 89, 1, 1, "Inverter Device Status", 0]
+  ["inverter_device_status", "uint16", 89, 1, 1, "Inverter Device Status", 0],
+  ["inverter_accumulated_p", "uint32", 106, 2, 0.01, "Accumulated energy yield", 0],
+  ["inverter_daily_p", "uint32", 114, 2, 0.01, "Daily energy yield", 0]
 ]
 
+meter_starting_address = 37100
+meter_register_count = 23
 meter_sensors = [
   ["meter_status", "uint16", 0, 1, 1, "Meter Status", 0],
   ["meter_v", "int32", 1, 2, 0.1, "Meter Voltage (V)", 0],
@@ -76,27 +90,63 @@ def recover_all_sensors(sensors, registers):
     sensor[6] = data
     print(sensor[5] + ": " + str(data) + data_hex)
 
-start_time = time.time()
-i = 0
 
-while True:
-    print(">>>> Iteration " + str(i))
-    i = i + 1
+def store_all_sensors(sensors, api):
+  for sensor in sensors:
+    p = Point(measurement).tag("version", measurement_version).field(sensor[0], sensor[6])
+    api.write(bucket=bucket, record=p)
 
-    client = ModbusTcpClient(host=inverter_host, port=inverter_port, unit_id=inverter_id)
-    client.connect()
 
-    if client.connect():
-      print(">> Connected to: " + inverter_host + ":" + str(inverter_port))
-      time.sleep(2)
+def gather_modbus_data():
+  modbus_client = ModbusTcpClient(host=inverter_host, port=inverter_port, unit_id=inverter_id)
 
-      inverter_data = client.read_holding_registers(32000, 90)
-      meter_data = client.read_holding_registers(37100, 23)
+  if modbus_client.connect():
+    print(">> MODBUS connected to: " + inverter_host + ":" + str(inverter_port))
+    time.sleep(modbus_wait_for_connnection)
 
-      recover_all_sensors(inverter_sensors, inverter_data.registers)
-      recover_all_sensors(meter_sensors, meter_data.registers)
+    inverter_data = modbus_client.read_holding_registers(inverter_starting_address, inverter_register_count)
+    meter_data = modbus_client.read_holding_registers(meter_starting_address, meter_register_count)
 
-      client.close()
-      print(">> Connection closed: " + inverter_host + ":" + str(inverter_port))
+    recover_all_sensors(inverter_sensors, inverter_data.registers)
+    recover_all_sensors(meter_sensors, meter_data.registers)
+
+    modbus_client.close()
+    print(">> MODBUS connection closed: " + inverter_host + ":" + str(inverter_port))
+
+    return True
+  else:
+    return False
+
+
+def store_influx_data():
+  influx_client = InfluxDBClient(url=influx_url, token=influx_token, org=influx_org)
+  
+  if influx_client.ready():
+    print(">> Influx connected to: " + influx_url)
+
+    write_api = influx_client.write_api(write_options=SYNCHRONOUS)
+
+    store_all_sensors(inverter_sensors, write_api)
+    store_all_sensors(meter_sensors, write_api)
+
+    influx_client.close()
+    print(">> Influx connection closed: " + influx_url)
+
+
+def main():
+
+  start_time = time.time()
+  i = 0
+
+  while True:
+    print(">>>> Iteration " + str(i)) 
+
+    if gather_modbus_data():
+      store_influx_data()    
 
     time.sleep(scheduling_seconds - ((time.time() - start_time) % scheduling_seconds))
+    i = i + 1
+
+
+if __name__ == "__main__":
+  main()
