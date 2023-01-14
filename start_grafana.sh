415c2d7769af2ad9c04befb5becb8b1906a08df0
Ignacio Sanchez Gines <nacho@geardome.com>
2023-01-14 13:15:40 +0100

    Grafana https and volume

--- start_grafana.sh
+++ start_grafana.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-sudo docker run --name grafana -d --restart always -p 3000:3000 grafana/grafana:latest
+sudo docker run --name grafana -d --restart always -p 3000:3000 -v grafana:/var/lib/grafana \
+  -e "GF_SECURITY_ALLOW_EMBEDDING=false" \
+  -e "GF_USERS_ALLOW_SIGN_UP=false" \
+  -e "GF_AUTH_ANONYMOUS_ENABLED=false" \
+  -e "GF_SERVER_PROTOCOL=https" \
+  -e "GF_SERVER_CERT_FILE=/var/lib/grafana/ssl/grafana.crt" \
+  -e "GF_SERVER_CERT_KEY=/var/lib/grafana/ssl/grafana.key" \
+  grafana/grafana:latest
